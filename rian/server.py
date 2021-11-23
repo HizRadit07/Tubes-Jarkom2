b20b7dea4d0e3178af75d35afa85db1746d1de75
solace6282 <gkataro4@gmail.com>
2021-11-23 21:41:26 +0700

    cocokin spek buat argv

--- rian/server.py
+++ rian/server.py
@@ -4,8 +4,10 @@ from threading import Thread, Event
 from time import sleep
 from constants import *
 from segment import *
+import sys
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = sys.argv[1]      # Port to listen on (non-privileged ports are > 1023)
+FILE_PATH=sys.argv[2]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 gas = False
