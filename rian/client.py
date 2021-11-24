98b5f9b5cfe70a6051d3a36a9a2884d676c83d55
abid123456 <abdilh_ahmad_official@yahoo.com>
2021-11-24 16:25:40 +0700

    Infrastruktur segment & fix 3wh

--- rian/client.py
+++ rian/client.py
@@ -4,8 +4,7 @@ from constants import *
 from segment import *
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = sys.argv[1]        # The port used by the server
-SAVE_PATH = sys.argv[2]
+PORT = 65432        # The port used by the server
 #ipt = sys.argv[1]
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- rian/server.py
+++ rian/server.py
@@ -4,10 +4,8 @@ from threading import Thread, Event
 from time import sleep
 from constants import *
 from segment import *
-import sys
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = sys.argv[1]      # Port to listen on (non-privileged ports are > 1023)
-FILE_PATH=sys.argv[2]
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 gas = False
